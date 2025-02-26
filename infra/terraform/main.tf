eca197e571c68401dedc7371fbdb053ed83859b4
parulgupta <parul.g@cogostech.com>
2025-02-26 20:18:59 +0530

    version correction

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.0"
+  version = "5.0.0"  # Updated to latest stable version
 
   name = "quest-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.region}a", "${var.region}b"]
+  azs             = ["${var.region}-a", "${var.region}-b"]  # Fixed AZ format
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -19,13 +19,13 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.21.0"
+  version = "20.0.0"  # Updated version for compatibility
 
   cluster_name    = "quest-cluster"
   cluster_version = "1.21"
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
+  subnet_ids = module.vpc.private_subnets  # EKS nodes should be in private subnets
 
   eks_managed_node_groups = {
     quest-node-group = {
